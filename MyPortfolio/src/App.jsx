5b0488f1868b15901d8183466490b430096a4bb4
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-09 20:31:10 -0400

    update: Post.jsx - testing props

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -5,7 +5,11 @@ export function App() {
   return (
     <div>
       <h1>My Portfolio</h1>
-      <Post />
+      <Post 
+        title="Post 1"
+        content="Content 1"
+        author="Author 1"
+      />
     </div>
   )
 }
