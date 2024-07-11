61dd77fb653aa46ddfbb40c44bcfa7ea60bcc6b2
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-10 20:49:40 -0400

    restart the project

--- MyPortfolio/src/Post.jsx
+++
@@ -1,9 +0,0 @@
-export function Post(props){
-    return(
-        <div>
-            <strong>{props.title}</strong>
-            <p>{props.content}</p>
-            <p>{props.author}</p>
-        </div>
-    )
-}
