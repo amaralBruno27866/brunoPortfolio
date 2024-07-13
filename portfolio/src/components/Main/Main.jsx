b8bb4c407ce3383ee7f0c2691a4f7971b3876d4d
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-13 15:03:26 -0400

    finishing the main component

--- portfolio/src/components/Main/Main.jsx
+++ portfolio/src/components/Main/Main.jsx
@@ -6,13 +6,15 @@ export function Main(){
             <video className={styles.video} autoPlay loop muted>
                 <source src='../../../video/bk_1.mp4'/>
             </video>
-            <div className={styles.content}>
-                <h4>test1</h4>
-                <h1>test2</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, modi! Tempora quasi eaque quae eius, omnis assumenda laborum odit reiciendis ab deserunt quia voluptatem nobis neque magnam doloremque ipsam cum!</p>
-            </div>
-            <div>
-                <img src="" alt="" />
+            <div className={styles.by2}>
+                <div className={styles.content}>
+                    <h4>test1</h4>
+                    <h1>test2</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, modi! Tempora quasi eaque quae eius, omnis assumenda laborum odit reiciendis ab deserunt quia voluptatem nobis neque magnam doloremque ipsam cum!</p>
+                </div>
+                <div>
+                    <img src="../../../img/coding.png" alt="a men coding" />
+                </div>
             </div>
         </div>
     )
