5bdec3d56f724ffae6e96307565e5d4971cab4cc
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-13 14:42:47 -0400

    add: video and other components

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -4,20 +4,22 @@ import logo from '../../../img/MyLogo.png';
 export function Header(){
     return (
         <header className={styles.header}>
+            <div className={styles.logo}>
+                <img src={logo} alt="My Logo" />
+            </div>
             <nav>
-                <div className={styles.logo}>
-                    <img src={logo} alt="My Logo" />
-                </div>
                 <div className={styles.menu}>
                     <ul>
                         <li>Home</li>
                         <li>Projects</li>
                         <li>About me</li>
-                        <li>Extra</li>
+                        <li>Certificates</li>
                     </ul>
-                    <button>Contract Me</button>
                 </div>
             </nav>
+            <div className={styles.button}>
+                <button>Contract Me</button>
+            </div>
         </header>
     );
 }
--- portfolio/src/components/Main/Main.jsx
+++ portfolio/src/components/Main/Main.jsx
@@ -3,7 +3,17 @@ import styles from './Main.module.css'
 export function Main(){
     return(
         <div className={styles.Main}>
-            <h1>Test</h1>
+            <video className={styles.video} autoPlay loop muted>
+                <source src='../../../video/bk_1.mp4'/>
+            </video>
+            <div className={styles.content}>
+                <h4>test1</h4>
+                <h1>test2</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, modi! Tempora quasi eaque quae eius, omnis assumenda laborum odit reiciendis ab deserunt quia voluptatem nobis neque magnam doloremque ipsam cum!</p>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
         </div>
     )
 }
