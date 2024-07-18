fab9a27bb8bdb7d877f026c718febbdd59d19568
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-18 19:09:28 -0400

    create a new project test

---
+++ myPortfolio#2/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <section>
+      <header>
+
+      </header>
+      <body>
+        
+      </body>
+      <footer>
+
+      </footer>
+    </section>
+  )
+}
+
+export default App
