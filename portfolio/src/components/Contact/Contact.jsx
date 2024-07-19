151162a1aef4076d1b7e24fb0f9b44047bc53d9e
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-19 15:36:52 -0400

    update

--- portfolio/src/components/Contact/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Contact.module.css';
-
-export function Contact() {
-    return (
-        <div className={styles.contact}>
-            <h1>Contact</h1>
-        </div>
-    )
-}
--- portfolio/src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.css';
-
-export function Footer() {
-    return (
-        <div className={styles.footer}>
-            <h1>Footer</h1>
-        </div>
-    )
-}
