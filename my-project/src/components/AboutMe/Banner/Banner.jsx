b1618e6e9cb6d24c1bb5608b703e530d49c6afcc
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-20 12:27:11 -0400

    update: applying styles

--- my-project/src/components/AboutMe/Banner/Banner.jsx
+++ my-project/src/components/AboutMe/Banner/Banner.jsx
@@ -9,8 +9,13 @@ export function Banner(){
                     <img src={photo} alt="" />
                 </figure>
                 <div className={styles.card_body}>
-                    <h2>New album is released!</h2>
-                    <p>Click the button to listen on Spotiwhy app.</p>
+                    <h2>Bella the Beagle</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Nulla recusandae reprehenderit voluptas esse, sequi eaque 
+                        voluptates reiciendis odit ratione rem, in, cupiditate non 
+                        eveniet temporibus cum optio quaerat vero eius.
+                    </p>
                     <div className={styles.button}>
                         <button> Turn </button>
                     </div>
