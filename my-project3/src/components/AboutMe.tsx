7ca09483f8f8df607cfb78e7b419b89d52f3f310
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-28 15:29:19 -0400

    update: component About Me was created

--- my-project3/src/components/AboutMe.tsx
+++ my-project3/src/components/AboutMe.tsx
@@ -1,9 +1,16 @@
 import styles from "../styles/AboutMe.module.css";
+import { Card } from "./Card";
 
 export function AboutMe() {
   return (
     <section id="About" className={styles.about_content}>
       <h1>About Me</h1>
+      <div className={styles.about_container}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </section>
   );
 }
---
+++ my-project3/src/components/Card.tsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Card.module.css";
+
+export function Card() {
+  return (
+    <section className={styles.card_content}>
+      <img src="" alt="" />
+      <h2>My own card</h2>
+      <p></p>
+      <button>bt</button>
+    </section>
+  );
+}
