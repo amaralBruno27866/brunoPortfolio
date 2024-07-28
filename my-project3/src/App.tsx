de03335359b02efdb235e685543cf831c40fb588
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-28 13:27:03 -0400

    create: new project using type script

--- my-project3/src/App.tsx
+++ my-project3/src/App.tsx
@@ -1,11 +1,10 @@
-import { Navbar } from "./components/NavBar/Navbar";
 import "./global.css";
 import styles from "./App.module.css";
 
 export function App() {
   return (
-    <div className={styles.main}>
-      <Navbar />
+    <div className={styles.wrapper}>
+      <h1>teste</h1>
     </div>
   );
 }
