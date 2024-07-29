816217e69bbe46d93c38462356c0851cdac732e2
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-29 19:57:59 -0400

    update: component card and about me

--- my-project3/src/components/Card.tsx
+++ my-project3/src/components/Card.tsx
@@ -1,12 +1,25 @@
 import styles from "../styles/Card.module.css";
+import pankeka from "../assets/Pankeka.png";
+import { ArrowUDownRight } from "phosphor-react";
 
 export function Card() {
   return (
     <section className={styles.card_content}>
-      <img src="" alt="" />
-      <h2>My own card</h2>
-      <p></p>
-      <button>bt</button>
+      <img src={pankeka} alt="Pankeka beagle" />
+      <div className={styles.container}>
+        <h2>Pankeka, the lemon beagle</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi eius
+          perspiciatis voluptates, aspernatur id cumque nemo minima accusantium
+          aut, molestiae dolorum impedit atque? Delectus eveniet officiis
+          voluptates ducimus deserunt ab!
+        </p>
+      </div>
+      <div className={styles.footer}>
+        <button id="turn">
+          <ArrowUDownRight size={32} />
+        </button>
+      </div>
     </section>
   );
 }
