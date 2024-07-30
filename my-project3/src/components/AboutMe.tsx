be56f5c0a79c6dda4b5baee23ecc1477f97b83f2
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-29 20:46:34 -0400

    update: component about

--- my-project3/src/components/AboutMe.tsx
+++ my-project3/src/components/AboutMe.tsx
@@ -1,32 +1,35 @@
-import styles from "../styles/AboutMe.module.css";
 import { Card } from "./Card";
+import styles from "../styles/AboutMe.module.css";
 import pankeka from "../assets/Pankeka.png";
+import "swiper/css";
 
 export function AboutMe() {
   return (
     <section id="About" className={styles.about_content}>
       <h1>About Me</h1>
-      <div className={styles.about_container}>
-        <Card
-          picture={<img src={pankeka} alt="Card Image" />}
-          title="Pankeka, the beagle"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, ipsam mollitia ducimus non pariatur in aliquid saepe culpa recusandae quidem repellat officiis. Quos suscipit nam ullam iusto vero molestiae nobis."
-        />
-        <Card
-          picture={<img src={pankeka} alt="Card Image" />}
-          title="Pankeka, the beagle"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, ipsam mollitia ducimus non pariatur in aliquid saepe culpa recusandae quidem repellat officiis. Quos suscipit nam ullam iusto vero molestiae nobis."
-        />
-        <Card
-          picture={<img src={pankeka} alt="Card Image" />}
-          title="Pankeka, the beagle"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, ipsam mollitia ducimus non pariatur in aliquid saepe culpa recusandae quidem repellat officiis. Quos suscipit nam ullam iusto vero molestiae nobis."
-        />
-        <Card
-          picture={<img src={pankeka} alt="Card Image" />}
-          title="Pankeka, the beagle"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, ipsam mollitia ducimus non pariatur in aliquid saepe culpa recusandae quidem repellat officiis. Quos suscipit nam ullam iusto vero molestiae nobis."
-        />
+      <div className={styles.about_containerAll}>
+        <div className={styles.about_containerOther}>
+          <h2>test</h2>
+        </div>
+        <div className={styles.about_containerCard}>
+          <Card
+            picture={<img src={pankeka} alt="Pankeka the beagle" />}
+            title="Pankeka, the Beagle"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Totam, quisquam. Illo mollitia laudantium eum ducimus alias corporis 
+          quas velit. Vel ut eveniet quaerat, vitae nulla iste dolorem nobis 
+          molestias quos?"
+          />
+
+          <Card
+            picture={<img src={pankeka} alt="Pankeka the beagle" />}
+            title="Pankeka, the Beagle"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Totam, quisquam. Illo mollitia laudantium eum ducimus alias corporis 
+          quas velit. Vel ut eveniet quaerat, vitae nulla iste dolorem nobis 
+          molestias quos?"
+          />
+        </div>
       </div>
     </section>
   );
